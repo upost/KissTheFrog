9980d90cf099f4a66c21dada09b5a326183eda57
upost <uwe.post@upcenter.de>
2016-06-29 17:34:21 +0200

    updated gradle

--- KissTheFrog/src/main/java/de/spas/kissthefrog/MainActivity.java
+++ KissTheFrog/src/main/java/de/spas/kissthefrog/MainActivity.java
@@ -1,14 +1,18 @@
 package de.spas.kissthefrog;
 
+import android.app.Activity;
 import android.app.Dialog;
 import android.content.SharedPreferences;
 import android.graphics.Typeface;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.os.Bundle;
-import android.app.Activity;
 import android.os.Handler;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -18,7 +22,7 @@ import java.util.Random;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
-    private static final int FROG_ID = 212121;
+
     private int points;
     private int round;
     private int countdown;
@@ -27,11 +31,17 @@ public class MainActivity extends Activity implements View.OnClickListener {
     private Handler handler = new Handler();
     private Typeface ttf;
     private ImageView frog;
+    private AudioManager audioManager;
+    private SoundPool soundPool;
+    private int frogSoundId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        audioManager = (AudioManager)getSystemService(AUDIO_SERVICE);
+        soundPool = new SoundPool(2,AudioManager.STREAM_MUSIC,1);
+        frogSoundId = soundPool.load(this, R.raw.frog, 1);
         ttf = Typeface.createFromAsset(getAssets(), "JandaManateeSolid.ttf");
         ((TextView)findViewById(R.id.countdown)).setTypeface(ttf);
         ((TextView)findViewById(R.id.round)).setTypeface(ttf);
@@ -70,7 +80,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
         container.addView(wv, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
         wv.setImageCount(8*(10+round));
         frog = new ImageView(this);
-        frog.setId(FROG_ID);
+        frog.setId(R.id.frog);
         frog.setImageResource(R.drawable.frog);
         frog.setScaleType(ImageView.ScaleType.CENTER);
         float scale = getResources().getDisplayMetrics().density;
@@ -117,10 +127,27 @@ public class MainActivity extends Activity implements View.OnClickListener {
     @Override
     public void onClick(View view) {
         if(view.getId()==R.id.start) {
-            startGame();
+            Animation a = AnimationUtils.loadAnimation(this,R.anim.pump);
+            a.setAnimationListener(new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
+
+                }
+
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    startGame();
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+
+                }
+            });
+            view.startAnimation(a);
         } else if(view.getId()==R.id.play_again) {
             showStartFragment();
-        } else if(view.getId()==FROG_ID) {
+        } else if(view.getId()==R.id.frog) {
             kissFrog();
         } else if(view.getId()==R.id.help) {
             showTutorial();
@@ -129,6 +156,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     private void kissFrog() {
         handler.removeCallbacks(runnable);
+        soundPool.play(frogSoundId,1,1,1,0,1);
         showToast(R.string.kissed);
         points += countdown*1000;
         round++;
